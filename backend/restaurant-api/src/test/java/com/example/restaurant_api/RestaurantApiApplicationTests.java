4bbe1dbff31be84f14edbc18cf7ffb1c17b54427
Jainam Chhatbar <jnchhatbar.jnc@gmail.com>
2025-04-30 23:31:29 -0700

    Cleanup environment setup

--- backend/restaurant-api/src/test/java/com/example/restaurant_api/RestaurantApiApplicationTests.java
+++ backend/restaurant-api/src/test/java/com/example/restaurant_api/RestaurantApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.restaurant_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class RestaurantApiApplicationTests {
 
 	@Test
